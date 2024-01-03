869bd1fb5592bf326e1e629a1546837d6c8b5828
chetan360 <chetansmankar360@gmail.com>
2024-01-03 12:29:05 +0530

    Added GameLoop and FPS limiter/ FPS Counter

--- PlatformerGame/src/main/GamePanel.java
+++ PlatformerGame/src/main/GamePanel.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -9,9 +12,18 @@ import inputs.MouseInputs;
 
 public class GamePanel extends JPanel{
 	private MouseInputs mouseInputs;
-	private int xDelta = 100, yDelta = 100;
+	private float xDelta = 100, yDelta = 100;
+	private float xDir = 1f, yDir = 1f;
+	private int frames = 0;
+	private long lastCheck = 0;
+	private Color color = new Color(150, 30, 35);
+	private Random random;
+	
+	//temp
+	private ArrayList<MyRect> rects = new ArrayList<>();
 	
 	public GamePanel() {
+		random = new Random();
 		mouseInputs = new MouseInputs(this);
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
@@ -20,23 +32,98 @@ public class GamePanel extends JPanel{
 	
 	public void changeXDelta(int value) {
 		this.xDelta += value;
-		repaint();
 	}
 	
 	public void changeYDelta(int value) {
 		this.yDelta += value;
-		repaint();
 	}
 	
 	public void setRectPos(int x, int y) {
 		this.xDelta = x;
 		this.yDelta = y;
-		repaint();
+	}
+	
+	public void spawnRect(int x, int y) {
+		rects.add(new MyRect(x, y));
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		g.fillRect(xDelta, yDelta, 200, 50);
+		//temp rects
+		for(MyRect rect: rects) {
+			rect.updateRect();
+			rect.draw(g);
+		}
+		
+		updateRectangle();
+		
+		g.setColor(color);
+		g.fillRect((int)xDelta, (int)yDelta, 200, 50);
+		
+	}
+
+	private void updateRectangle() {
+		xDelta += xDir;
+		if(xDelta > 400 || xDelta < 0) {
+			//reverse the x directions
+			xDir *= -1;
+			color = getRndColor();
+		}
+		
+		yDelta += yDir;
+		if(yDelta > 400 || yDelta < 0) {
+			//reverse the y directions
+			yDir *= -1;
+			color = getRndColor();
+		}
+	}
+
+	private Color getRndColor() {
+		int r = random.nextInt(255);
+		int b = random.nextInt(255);
+		int g = random.nextInt(255);
+		
+		return new Color(r, b, g);
+	}
+	
+	//temp
+	public class MyRect {
+		int x, y, w, h;
+		int xDir = 1, yDir = 1;
+		Color color;
+		
+		public MyRect(int x, int y) {
+			this.x = x;
+			this.y = y;
+			w = random.nextInt(50);
+			h = w;
+			color = newColor();
+		}
+		
+		public void updateRect() {
+			this.x += xDir;
+			this.y += yDir;
+			
+			if((x+w) > 400 || x < 0) {
+				xDir *= -1;
+				color = newColor();
+			}
+			
+			if((y+h) > 400 || y < 0) {
+				yDir *= -1;
+				color = newColor();
+			}
+			
+		}
+		
+		private Color newColor() {
+			return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+		}
+		
+		public void draw(Graphics g) {
+			g.setColor(color);
+			g.fillRect(x, y, w, h);
+		}
 	}
 }
