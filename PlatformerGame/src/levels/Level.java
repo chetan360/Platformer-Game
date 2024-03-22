2d6520282eb02745a6351c6f2f8d77992d31c54c
chetan360 <chetansmankar360@gmail.com>
2024-03-22 23:05:40 +0530

    added cannons in Game

--- PlatformerGame/src/levels/Level.java
+++ PlatformerGame/src/levels/Level.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import entities.Crabby;
 import main.Game;
+import objects.Cannon;
 import objects.GameContainer;
 import objects.Potion;
 import objects.Spike;
@@ -24,7 +25,8 @@ public class Level {
 	private ArrayList<Potion> potions;
 	private ArrayList<Spike> spikes;
 	private ArrayList<GameContainer> containers;
-
+	private ArrayList<Cannon> cannons;
+	
 	private int lvlTilesWide;
 	private int maxTilesOffset;
 	private int maxLvlOffsetX;
@@ -37,8 +39,13 @@ public class Level {
 		createPotions();
 		createContainers();
 		createSpikes();
+		createCannons();
 		calcLvlOffset();
 		calcPlayerSpawn();
+	}
+
+	private void createCannons() {
+		cannons = HelpMethods.GetCannons(img);
 	}
 
 	private void createSpikes() {
@@ -101,5 +108,9 @@ public class Level {
 	
 	public ArrayList<Spike> getSpikes() {
 		return spikes;
+	}
+	
+	public ArrayList<Cannon> getCannons() {
+		return cannons;
 	}
 }
